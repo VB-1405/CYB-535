84d33c846665c0f9333ec2ee7a8e0b110444d601
Vrishabh Bhavsar <vrishabhbhavsar@gmail.com>
2026-02-19 14:40:20 -0800

    refactor: replace old project with Basic Python utilities

--- helpful_scripts/kubekill.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl config delete-context kind-dev-cluster || echo "Could not delete kubernetes config file"
-kind delete cluster --name "dev-cluster" || echo "Could not kill dev-cluster"
